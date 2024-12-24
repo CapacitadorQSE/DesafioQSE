aaa2bd355ff1dc5de24c4e33ec420b3e233f8c24
Capacitador <capacitadorqse@gmail.com>
2024-12-24 12:21:59 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+document.querySelectorAll('nav a').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop - 50, // Considerando o cabeçalho fixo
+        behavior: 'smooth'
+      });
+    }
+  });
+});
